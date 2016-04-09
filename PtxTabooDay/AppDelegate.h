0f86e31f883e3b911dd22b2fd1cf7f31717fc500
fido0725 <chenfong_12@163.com>
2016-04-09 22:52:24 +0800

    Initial Commit

---
+++ PtxTabooDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PtxTabooDay
+//
+//  Created by ptx on 16/4/9.
+//  Copyright © 2016年 ptx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
